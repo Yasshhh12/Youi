f3475eaaa253732c3efd0c62c0e7977577e7722e
Yasshhh12 <prasyast152@gmail.com>
2024-02-09 02:57:21 +0530

    Done with the home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box,Heading,Image } from '@chakra-ui/react'
+import { Box,Heading,Image,Container, Stack,Text } from '@chakra-ui/react'
 import React from 'react'
 import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css"
@@ -9,6 +9,7 @@ import img4 from "../images/4.jpg"
 import img5 from "../images/5.png"
 
 const HeadingOptions={
+  
     pos:"absolute",
     left:"50%",
     top:"50%",
@@ -16,12 +17,32 @@ const HeadingOptions={
     textTransform:"uppercase",
     p:'4',
     size:'4xl'
+  
 }
 
 const Home = () => {
   return (
     <Box>
       <MyCarousel />
+      <Container maxW={'container.xl'} minH={'100vh'} p='16'>
+        <Heading textTransform={'uppercase'} py='2' w={'fit-content'} borderBottom={'2px solid'} m='auto'>
+          Services
+        </Heading>
+
+        <Stack h="full" p={'4'} alignItems={'center'} direction={['coloumn','row']}>
+         <Image src={img5} h={['40','400']} filter={'hue-rotate(-130deg)'}/>
+         <Text letterSpacing={'widest'} lineHeight={'190%'} p={['4','16']} textAlign={'center'}>
+         Discover a world of gaming excellence with our diverse range of services. 
+         Dive into expert game reviews and recommendations, engage with our lively community forums for tips and strategies, 
+         and explore comprehensive game guides and walkthroughs to conquer every challenge. 
+         Participate in thrilling gaming events and tournaments, explore our curated collection of gaming merchandise, 
+         and enjoy captivating live gameplay and tutorials from top streamers. 
+         Need assistance? Our prompt technical support is here to help. Plus, unlock exclusive discounts on 
+         games and gear and stay updated with the latest gaming news. 
+         Join our vibrant community today and elevate your gaming experience to new heights.
+         </Text>
+        </Stack>
+      </Container>
     </Box>
   )
 }
